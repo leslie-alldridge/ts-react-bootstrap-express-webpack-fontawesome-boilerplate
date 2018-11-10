2a7ab8cf608e018791988e0e24bf11bf370ffdc5
leslie-alldridge <leslie.alldridge@gmail.com>
2018-11-11 09:23:01 +1300

    init

---
+++ README.md
@@ -0,0 +1,39 @@
+# Boilerplate
+
+### Stack
+
+React (packages for redux, router are included but not set up in the example)
+
+Webpack
+
+Font Awesome
+
+React-Bootstrap
+
+Typescript
+
+Express
+
+Heroku (deploy)
+
+## Installation
+
+```
+$ yarn
+```
+
+## Running
+
+```
+$ yarn start
+```
+
+Dev mode
+
+```
+$ yarn dev
+```
+
+## Info
+
+Files will be added to the 'build' folder. Webpack dev server is included, but not set up - so pages will require manual refresh (Ctrl + R).
---
+++ src/app/containers/App/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as style from './style.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { ContactContent } from 'app/components';
+
+library.add(fab, fas);
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div className={style.new}>
+        {/* your components go here */}
+        <ContactContent />
+      </div>
+    );
+  }
+}
